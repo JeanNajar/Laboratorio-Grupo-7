7b7fd49c008353e61995c398a2590eb4d7b4b33e
Debbido27 <davidmedina272004@gmail.com>
2026-01-30 08:51:53 -0600

    Se agrega los atributos de la clase, el constructor, y tambien los getter necesarios

--- Laboratorio2_30_1_2026/src/laboratorio2_30_1_2026/EMPLEADO.java
+++ Laboratorio2_30_1_2026/src/laboratorio2_30_1_2026/EMPLEADO.java
@@ -1,13 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package laboratorio2_30_1_2026;
 
-/**
- *
- * @author Dell
- */
+package laboratorio2_30_1_2026;
+import java.time.LocalDate;
+
 public class EMPLEADO {
+    
+    protected String codigo;
+    protected String nombre;
+    protected LocalDate fechaContratacion;
+    protected double salarioBase;
+    protected double horasTrabajadas;
+    
+    public EMPLEADO(String codigo, String nombre, double salarioBase){
+        this.codigo=codigo;
+        this.nombre=nombre;
+        this.fechaContratacion = LocalDate.now();
+        this.salarioBase=salarioBase;
+        this.horasTrabajadas=horasTrabajadas;
+    }
+    
+    public String getCodigo(){
+        return codigo;
+    }
+    
+    public double getHorasTrabajadas(){
+        return horasTrabajadas;
+    }
+    
+    
     
 }
