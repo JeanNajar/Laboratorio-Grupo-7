a728fe756814e7722eaf622fc35171c181dadb0a
Debbido27 <davidmedina272004@gmail.com>
2026-01-30 09:32:37 -0600

    Se agregaron los atributos, y se agrego el constructor de empleado

--- Laboratorio2_30_1_2026/src/laboratorio2_30_1_2026/Empresa.java
+++ Laboratorio2_30_1_2026/src/laboratorio2_30_1_2026/Empresa.java
@@ -6,6 +6,11 @@ public class Empresa {
     
     public EMPLEADO[] empleados;
     private int contador;
+     
+    public Empresa(int capacidadMaxima){
+        empleados=new EMPLEADO[capacidadMaxima];
+        contador=0;
+    }
     
  
 }
