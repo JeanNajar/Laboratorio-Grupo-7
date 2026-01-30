c8820d46dcc86b62af8c6cecf756257117479c8f
JeanNajar <jean_carlos@unitec.edu>
2026-01-30 09:56:42 -0600

    claseEmpleadoVentas 1.1

--- Laboratorio2_30_1_2026/src/laboratorio2_30_1_2026/EmpleadoVentas.java
+++ Laboratorio2_30_1_2026/src/laboratorio2_30_1_2026/EmpleadoVentas.java
@@ -1,13 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package laboratorio2_30_1_2026;
 
-/**
- *
- * @author Dawnfall
- */
-public class EmpleadoVentas {
+import java.time.LocalDate;
+
+public class EmpleadoVentas extends EMPLEADO{
+    
+private double[] ventasMensuales;
+private double tasaComision;
+
+    public EmpleadoVentas(String codigo, String nombre, double salarioBase) {
+        super(codigo, nombre, salarioBase);
+        this.tasaComision= tasaComision;
+        this.ventasMensuales= new double[12]; 
+    }
+ 
+    public void registrarVentas(double monto){
+        if (monto > 0) {
+            int mesActual = LocalDate.now().getMonthValue() - 1;
+            ventasMensuales[mesActual] += monto;
+        }
+    }
+     public double calcularComision() {
+        int mesActual = LocalDate.now().getMonthValue() - 1;
+        return ventasMensuales[mesActual] * tasaComision;
+    }
+     @Override
+    public double calcularPago() {
+        double salarioProporcional = (salarioBase / 160) * horasTrabajadas;
+        return salarioProporcional + calcularComision();
+    }
+    
+    public double calcularVentasAnuales() {
+        double total = 0;
+        for (double venta : ventasMensuales) {
+            total += venta;
+        }
+        return total;
+    }
+     @Override
+    public String mostrarInformacion() {
+        return super.mostrarInformacion() +
+               "\nVentas anuales: " + calcularVentasAnuales();
+    
+    }
+    
+    
     
 }
